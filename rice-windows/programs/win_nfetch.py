8037876329705e43f15490d68570235bb0639ae5
Pedro <72710233+tsukum0@users.noreply.github.com>
2025-07-27 20:43:50 -0300

    Update win_nfetch.py

--- rice-windows/programs/win_nfetch.py
+++ rice-windows/programs/win_nfetch.py
@@ -5,11 +5,6 @@ import time
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VENV_PATH = os.path.join(BASE_DIR, "venv")
 PYTHON_EXEC = os.path.join(VENV_PATH, "Scripts" if os.name == "nt" else "bin", "python")
-
-# Verifica se está rodando dentro do venv
-if sys.executable != PYTHON_EXEC:
-    print("[ERRO] Execute este script via iniciar.bat para garantir o ambiente virtual ativo.")
-    sys.exit(1)
     
 import platform
 import psutil
